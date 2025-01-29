a41e84241cf8fb8a980c080a82ab4e6199732798
net <pongsiri613@gmail.com>
2025-01-29 17:59:35 +0700

    Update

--- prediction.py
+++ prediction.py
@@ -1,45 +1,51 @@
+## Making Pridcition return class & prob
 from typing import List, Tuple
 import torch
 import torchvision.transforms as T
 
 from PIL import Image
-
 def pred_class(model: torch.nn.Module,
-                image, class_names: List[str],
-                image_size: Tuple[int, int] = (224, 224)):
-    # Open image
-    img = image 
-
-    # Create transformation for image (if one doesn't exist)
-    image_transform = T.Compose([
-        T.Resize(image_size),
-        T.ToTensor(),
-        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-
-    # Predict on image
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+                        image,
+                        class_names: List[str],
+                        image_size: Tuple[int, int] = (224, 224),
+                        ):
     
-    #Turn on model evaluation mode and inference mode
+    
+    # 2. Open image
+    img = image
+
+    # 3. Create transformation for image (if one doesn't exist)
+    image_transform = T.Compose([
+            T.Resize(image_size),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+        ])
+    
+
+    ### Predict on image ### 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # 4. Make sure the model is on the target device
     model.to(device)
+    
+
+    # 5. Turn on model evaluation mode and inference mode
+    model.half()
     with torch.inference_mode():
+      # 6. Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
+      transformed_image = image_transform(img).unsqueeze(dim=0).float()
 
-        #Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
-        transformed_image = image_transform(img).unsqueeze(dim=0).float()
+      # 7. Make a prediction on image with an extra dimension and send it to the target device
+      target_image_pred = model(transformed_image.to(device))
 
-        #Make a prediction on image with an extra dimension and send it to the target device
-        target_image_pred = model(transformed_image.to(device))
-
-    #Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    # 8. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
     target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
 
-     #Convert prediction probabilities -> prediction labels
-    target_image_pred_labels = target_image_pred_probs.argmax(dim=1)
+    # 9. Convert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
 
-    class_name = class_names[target_image_pred_labels]
-
+    classname =  class_names[target_image_pred_label]
     prob = target_image_pred_probs.cpu().numpy()
 
-    return class_name, prob
-
-    
+    return prob
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,7 @@ from prediction import pred_class
 import numpy as np
 
 # Set title 
-st.title('Tomato disease classification')
-
+st.title('Tomato Leaf Classification')
 #Set Header 
 st.header('Please up load picture')
 
@@ -14,6 +13,7 @@ st.header('Please up load picture')
 #Load Model 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = torch.load('mobilenetv3_large_100_checkpoint_fold0.pt', map_location=device)
+# model = model.to(torch.float32)
 
 
 
@@ -22,10 +22,10 @@ uploaded_image = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png']
 
 if uploaded_image is not None:
     image = Image.open(uploaded_image).convert('RGB')
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
     
-    class_name = ['Bacterial spot', 'Early blight', 'Late blight', 'Leaf Mold', 'Septoria leaf spot', 'Spider mites', 'Target Spot', 'Tomato Yellow Leaf Curl Virus', 'Tomato mosaic virus', 'healthy',
-                  'Mosaic Virus']
+    class_name = ['Bacterial_spot', 'Early_blight', 'Late_blight', 'Leaf_Mold', 'Septoria_leaf_spot',
+               'Spider_Mites', 'Target_Spot', 'Yellow_Leaf_Curl_Virus', 'healthy', 'mosaic_virus']
 
     if st.button('Prediction'):
         #Prediction class
@@ -40,3 +40,4 @@ if uploaded_image is not None:
             # Set the color to blue if it's the maximum value, otherwise use the default color
             color = "blue" if i == max_index else None
             st.write(f"## <span style='color:{color}'>{class_name[i]} : {probli[0][i]*100:.2f}%</span>", unsafe_allow_html=True)
+
